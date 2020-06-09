8c192d845a5dc50c2c2129f59b604bd2a708ccf2
kurt <vonehrk@umich.edu>
2020-06-09 20:46:07 +0000

    updated index to request info from API, updated server to respond to GET requests

--- apps/REST_API/server.js
+++ apps/REST_API/server.js
@@ -9,12 +9,12 @@ const app = express();
 app.listen(8000);
 
 //handler for incoming GET requests
-app.get('/', function(req, res) {
-	res.send('It works');
-});
+app.get('/', function (req, res, next) {
+  res.json({sector:"Food Retail",company:"Kroger",unique_id:1,street_address:"2641 Plymouth Rd",city:"Ann Arbor",state:"Michigan",current_occupancy:3})
+})
 
 //handler for incoming POST requests
 app.post('/api',function(req,res){
 	console.log(req);
-	res.send('POST request to the homepage')
+	res.send('POST Request')
 })
