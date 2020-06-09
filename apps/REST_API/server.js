749990a5f719188107e69294c0f1a7e6e8916a2f
kurt <vonehrk@umich.edu>
2020-06-09 18:33:17 +0000

    updated homepage with javascript to request default store for alpha release

--- apps/REST_API/server.js
+++ apps/REST_API/server.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var app = express();
+//import the express library
+const express = require('express');
+//instantiate the express module
+const app = express();
 
+//create the server port for listening to incoming HTTP requests
+//the traffic is routed from Port 80 to Port 8000 by configuring 
+//Apache with a reverse proxy on requests to the /api endpoint
+app.listen(8000);
+
+//handler for incoming GET requests
 app.get('/', function(req, res) {
 	res.send('It works');
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+//handler for incoming POST requests
+app.post('/api',function(req,res){
+	console.log(req);
+	res.send('POST request to the homepage')
+})
