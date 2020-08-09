a4552049c06cf66122cd87675771311fa6e75704
kurt <vonehrk@umich.edu>
2020-08-09 15:29:17 -0400

    updated omega post handling

--- apps/REST_API/omega_server.js
+++ apps/REST_API/omega_server.js
@@ -40,7 +40,7 @@ app.post('/', function (req, res, next) {
 //handler for incoming POST requests
 app.post('/store_management', function (req, res, next) {
 
-	setStoreOcc(req.body.unique_id, req.body.max_occupancy, req.body.comments,);
+	setStoreSettings(req.body.unique_id, req.body.max_occupancy, req.body.comments);
 	res.send('OK');
 })
 
@@ -123,6 +123,48 @@ async function setStoreOcc(storeID, current_occupancy) {
 
         // Find associated data for the given store ID
         store_data = await client.db("stores").collection("stores").updateOne(store, {$set: occupancy});
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+
+async function setStoreSettings(storeID, max_occupancy, comments) {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to re$
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb://127.0.0.1:27017/stores";
+
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.3/api/MongoClient.html $
+     */
+    const client = new MongoClient(uri);
+
+    var store = new Object();
+    store.unique_id = storeID;
+
+    var comments = new Object();
+	comments.comments = comments;
+
+    var max_occupancy = new Object();
+    max_occupancy.max_occupancy = max_occupancy;
+
+
+    console.log(store);
+    console.log(max_occupancy);
+    console.log(comments);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Find associated data for the given store ID
+        store_data = await client.db("stores").collection("stores").updateOne(store, {$set: {"max_occupancy": max_occupancy, "comments": comments}});
     } catch (e) {
         console.error(e);
     } finally {
